1b2886e850720cf4505d5611914f2465617fe03f
Kristinafffff <k.filipavice@gmail.com>
2022-10-09 17:35:03 +0300

    add task 4

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,21 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+function showData() {
+    fetch(ENDPOINT)
+      .then((res) => res.json())
+      .then((cars) => {
+        let result = "";
+        for (let i = 0; i < cars.length; i++) {
+          result +=
+            "<b>Gamintojas:</b> " +
+            cars[i].brand +
+            "</br> <b>Pagaminti automobiliai:w </b>" +
+            cars[i].models.toString() +
+            "</br></br>";
+        }
+        document.getElementById("output").innerHTML = result;
+      });
+  }
+  showData();
